c95a477f661ff3271e56a3d2e9cd5ea1f9c57e31
anuja <anujakoralage@gmail.com>
2020-02-07 23:10:42 +0530

    add web secrity configs

--- profile-service/src/main/java/com/anuja/rentcloud/profileservice/controller/ProfileController.java
+++ profile-service/src/main/java/com/anuja/rentcloud/profileservice/controller/ProfileController.java
@@ -3,17 +3,22 @@ package com.anuja.rentcloud.profileservice.controller;
 import com.anuja.rentcloud.commons.model.Customer;
 import com.anuja.rentcloud.profileservice.services.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin
 @RestController
 @RequestMapping(value = "/services")
 public class ProfileController {
     @Autowired
     CustomerService customerService;
 
+    @PostMapping
     public Customer save (@RequestBody Customer customer) {
         return customerService.save(customer);
+    }
+    @GetMapping
+    public Customer get(){
+        return new Customer(9,"aw","ko","ko98","ko99");
     }
 }
--- profile-service/src/main/java/com/anuja/rentcloud/profileservice/repository/CustomerRepository.java
+++ profile-service/src/main/java/com/anuja/rentcloud/profileservice/repository/CustomerRepository.java
@@ -2,6 +2,8 @@ package com.anuja.rentcloud.profileservice.repository;
 
 import com.anuja.rentcloud.commons.model.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Component;
 
+@Component
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
 }
